e4e6cbab5bd509e73ce6b05052a077ff97e9f4e0
A Machalica <a.machalica@selleo.com>
2015-11-04 17:47:49 +0100

    Unfinished

--- chapter2/recipe4/js/app.js
+++ chapter2/recipe4/js/app.js
@@ -1,10 +1,33 @@
-function MyCtrl($scope) {
-  $scope.name = "";
+module = angular.module('nameApp', []);
 
-  $scope.$watch("name", function() {
-    if ($scope.name.length > 0) {
-      $scope.greeting = "Greetings " + $scope.name;
+module.controller('MyCtrl', function ($scope) {
+    $scope.name = "";
+    $scope.visibility = false;
+
+
+    $scope.$watch("name", function () {
+        if ($scope.name.length > 0) {
+            $scope.greeting = "Greetings " + $scope.name;
+        }
+    });
+})
+;
+
+module.directive('paster', function () {
+        return {
+            link: function (scope, element, attributes) {
+                scope.$watch(attributes.show, function (value) {
+                    if (Math.abs(newVal.length - oldVal.length) > 1) {
+                        element.css('display', '');
+                    }
+                    else {
+                        element.css('display', 'none');
+                    }
+                    }
+                )
+                ;
+            }
+        }
     }
-  });
-
-}
+)
+;
